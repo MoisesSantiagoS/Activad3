fbd8d40b84ecef179c1bb80634fc6b7d852fc490
MoisesSantiagoS <82617245+MoisesSantiagoS@users.noreply.github.com>
2021-04-15 17:03:57 -0500

    Actividad 3

---
+++ Verificacion de Clave/Verificacion de Clave/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Verificacion_de_Clave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte K, SW;
+            string clave;
+            SW = 0; K = 0;
+
+            do
+            {
+                Console.Write("Digite su clave: "); clave = Console.ReadLine();
+                if ((clave.ToUpper() == "ARIEL"))
+                {
+                    SW = 1;
+                }
+                else
+                {
+                    K++;
+                }
+            } while (((K < 3) & (SW == 0)));
+
+            if (SW == 1)
+            {
+                Console.WriteLine("Bienvenido " + clave);
+            }
+            else
+            {
+                Console.WriteLine("Oportunidades Terminadas");
+
+            }
+        }
+    }
+}
